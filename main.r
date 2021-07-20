c24eede87d101f6aab4fab9e196b62c89a4562ab
Mrunmay Shelar <mrunmayshelar@gmail.com>
2021-07-20 10:15:40 +0530

    Till word cloud

--- main.r
+++ main.r
@@ -13,125 +13,102 @@ library(ggplot2)
 library(pals)
 
 extract <- function(filename) {
-  dataset = read.delim(filename, 
-                       header = FALSE, 
-                       sep = '\t', 
-                       quote = '', 
-                       dec = ' ', 
-                       stringsAsFactors = FALSE)
-  
+  dataset <- read.delim(filename,
+    header = FALSE,
+    sep = "\t",
+    quote = "",
+    dec = " ",
+    stringsAsFactors = FALSE
+  )
+
   dataset <- as.data.frame(dataset)
   row_index <- seq_len(nrow(dataset)) %% 2
-  details <- dataset[row_index == 1,]
-  abstract <- dataset[row_index == 0,]
+  details <- dataset[row_index == 1, ]
+  abstract <- dataset[row_index == 0, ]
   abstract <- as.data.frame(abstract)
   details <- as.data.frame(details)
-  
+
   doi <- extract_doi(details)
   author <- extract_author(details)
   title <- extract_title(details)
-  
+
   article <- data.frame(doi, title, author, abstract)
   colnames(article) <- c("DOI Link", "Title", "Author", "Abstract")
-  
+
   return(article)
 }
 
-extract_doi <- function(details_col){
+extract_doi <- function(details_col) {
   pattern_doi <- "(\\w+):\\s(\\d+)[^ab  c](\\d+)[^abc](\\d+)[^abc](\\d+)[^abc](\\d+)[^abc](\\d+)"
-  doi <- str_match(details_col[, 1],pattern_doi)
+  doi <- str_match(details_col[, 1], pattern_doi)
   doi <- as.data.frame(doi[, 1])
   return(doi)
-  
 }
 
 extract_author <- function(details_col) {
   pattern_author <- "([A-Z][a-z]+)\\s([A-Z]*(\\.*))\\w+\\s(\\w+\\s*\\w*)(\\..)"
-  author_name <- str_match(details_col[,1], pattern_author)
+  author_name <- str_match(details_col[, 1], pattern_author)
   author <- as.data.frame(author_name[, 1])
   return(author)
 }
 
-extract_title <- function(details_col){
+extract_title <- function(details_col) {
   pattern_title <- "(?<=\")(.*?)(?=\")"
   title <- str_match(details_col[, 1], pattern_title)
   title <- as.data.frame(title[, 1])
   return(title)
-  
 }
 
-dfn <-extract('citation.txt')
+dfn <- extract("citations.txt")
 
 load("data_common_words.RData")
 
- 
-calculateDTM <- function(dataframe) {
-  Corpus <- VCorpus(VectorSource(dataframe))   
-  toSpace <- content_transformer(
-    function (x, pattern)
-      gsub(pattern, " ", x))
-  processedCorpus <- tm_map(Corpus, toSpace, "/")
-  processedCorpus <- tm_map(Corpus, toSpace, "@")
-  processedCorpus <- tm_map(Corpus, toSpace, "#")
-  
-  
-  processedCorpus <- tm_map(processedCorpus, content_transformer(tolower))
-  processedCorpus <- tm_map(processedCorpus, removeNumbers)
-  processedCorpus <- tm_map(processedCorpus, removeWords, stopwords("english"))
-  processedCorpus <- tm_map(processedCorpus, removeWords, data_common_words)
-  processedCorpus <- tm_map(processedCorpus, stripWhitespace)
-  
-  
-  dtm <- TermDocumentMatrix(processedCorpus)
+
+calculatedtm <- function(dataframe) {
+  corpus <- VCorpus(VectorSource(dataframe))
+  tospace <- content_transformer(
+    function(x, pattern) {
+      gsub(pattern, " ", x)
+    }
+  )
+  processedcorpus <- tm_map(corpus, tospace, "/")
+  processedcorpus <- tm_map(processedcorpus, tospace, "@")
+  processedcorpus <- tm_map(processedcorpus, tospace, "#")
+
+
+  processedcorpus <- tm_map(processedcorpus, content_transformer(tolower))
+  processedcorpus <- tm_map(processedcorpus, removeNumbers)
+  processedcorpus <- tm_map(processedcorpus, removeWords, stopwords("english"))
+  processedcorpus <- tm_map(processedcorpus, removeWords, data_common_words)
+  processedcorpus <- tm_map(processedcorpus, stripWhitespace)
+
+
+  dtm <- TermDocumentMatrix(processedcorpus)
   return(dtm)
 }
 
-dtm <- calculateDTM(dfn$Abstract)
+dtm <- calculatedtm(dfn$Abstract)
 
-VCorpus <- VCorpus(VectorSource(df$Abstract))   
-VCorpus <- Corpus(DataframeSource(df$Abstract))
-processedCorpus <- tm_map(VCorpus, content_transformer(tolower))
-processedCorpus <- tm_map(processedCorpus, removeNumbers)
-processedCorpus <- tm_map(processedCorpus, removePunctuation, preserve_intra_word_dashes <- TRUE)
-processedCorpus <- tm_map(processedCorpus, removeWords, stopwords("english"))
-processedCorpus <- tm_map(processedCorpus, removeWords, data_common_words)
-processedCorpus <- tm_map(processedCorpus, stripWhitespace)
-minimumFrequency <- 5
-DTM <- DocumentTermMatrix(processedCorpus, control = list(bounds = list(global = c(minimumFrequency, Inf))))
-dim(DTM)
+calculatefreq <- function(dtm) {
+  dtm_matrix <- as.matrix(dtm)
+  v <- sort(rowSums(dtm_matrix), decreasing = TRUE)
+  d <- data.frame(word = names(v), freq = v)
 
+  return(d)
+}
+freqtable <- calculatefreq(dtm)
 
-sel_idx <- slam::row_sums(DTM) > 0
-DTM <- DTM[sel_idx, ]
-textdata <- textdata[sel_idx, ]
+View(freqtable)
+makewordcloud <- function(x) {
+  wordcloud(
+    words = x$word,
+    freq = x$freq,
+    min.freq = 1,
+    max.words = 250,
+    random.order = FALSE,
+    rot.per = 0.35,
+    colors = brewer.pal(8, "Dark2")
+  )
+}
 
-K <- 10
-topicModel <- LDA(DTM, K)
-  
-
-tmResult <- posterior(topicModel)
-attributes(tmResult)
-nTerms(DTM)   
-betas <- tmResult$terms  
-dim(beta)  
-thetas <- tmResult$topics 
-
-rowSums(theta)[1:10] 
-terms(topicModel, 10)
-
-topics <- tidy(topicModel, matrix = "beta")
-topics
-  
-top_terms <- topics %>%
-  group_by(topic) %>%
-  slice_max(beta, n = 10) %>% 
-  ungroup() %>%
-  arrange(topic, -beta)
-
-top_terms %>%
-  mutate(term = reorder_within(term, beta, topic)) %>%
-  ggplot(aes(beta, term, fill = factor(topic))) +
-  geom_col(show.legend = FALSE) +
-  facet_wrap(~ topic, scales = "free") +
-  scale_y_reordered()  
-
+makewordcloud(freqtable)
--- pubtator.r
+++ pubtator.r
@@ -1,14 +1,15 @@
-df<-  read.delim('mutation2pubtatorcentral.sample.txt', 
-                     header = FALSE, 
-                     sep = '\t', 
-                     quote = '', 
-                     dec = ' ', 
-                     stringsAsFactors = FALSE)
+df <- read.delim("mutation2pubtatorcentral.sample.txt",
+    header = FALSE,
+    sep = "\t",
+    quote = "",
+    dec = " ",
+    stringsAsFactors = FALSE
+)
 
-saveRDS(df, file='cellline2pubtator.Rda')
+saveRDS(df, file = "cellline2pubtator.Rda")
 
 
-dfnew <- readRDS('cellline2pubtator.Rda')
+dfnew <- readRDS("cellline2pubtator.Rda")
 
 
 
@@ -17,4 +18,49 @@ library(dplyr)
 library(stringi)
 
 
-pattern <- 
+VCorpus <- VCorpus(VectorSource(df$Abstract))
+VCorpus <- Corpus(DataframeSource(df$Abstract))
+processedCorpus <- tm_map(VCorpus, content_transformer(tolower))
+processedCorpus <- tm_map(processedCorpus, removeNumbers)
+processedCorpus <- tm_map(processedCorpus, removePunctuation, preserve_intra_word_dashes <- TRUE)
+processedCorpus <- tm_map(processedCorpus, removeWords, stopwords("english"))
+processedCorpus <- tm_map(processedCorpus, removeWords, data_common_words)
+processedCorpus <- tm_map(processedCorpus, stripWhitespace)
+minimumFrequency <- 5
+DTM <- DocumentTermMatrix(processedCorpus, control = list(bounds = list(global = c(minimumFrequency, Inf))))
+dim(DTM)
+
+
+sel_idx <- slam::row_sums(DTM) > 0
+DTM <- DTM[sel_idx, ]
+textdata <- textdata[sel_idx, ]
+
+K <- 10
+topicModel <- LDA(DTM, K)
+
+
+tmresult <- posterior(topicModel)
+attributes(tmResult)
+nTerms(DTM)
+betas <- tmResult$terms
+dim(beta)
+thetas <- tmResult$topics
+
+rowSums(theta)[1:10]
+terms(topicModel, 10)
+
+topics <- tidy(topicModel, matrix = "beta")
+topics
+
+top_terms <- topics %>%
+    group_by(topic) %>%
+    slice_max(beta, n = 10) %>%
+    ungroup() %>%
+    arrange(topic, -beta)
+
+top_terms %>%
+    mutate(term = reorder_within(term, beta, topic)) %>%
+    ggplot(aes(beta, term, fill = factor(topic))) +
+    geom_col(show.legend = FALSE) +
+    facet_wrap(~topic, scales = "free") +
+    scale_y_reordered()
